aaeed6f92e6cbeb15f84fbc766e87c3bd6a1e615
Berlin Ortega <marlitros200002@gmail.com>
2025-06-17 13:12:41 -0600

    Cambios en cors y desde donde se tiene permiso

--- src/main/java/com/backend/projectbackend/util/security/SecurityConfig.java
+++ src/main/java/com/backend/projectbackend/util/security/SecurityConfig.java
@@ -37,7 +37,7 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/auth/**").permitAll()
+                        .requestMatchers("/auth/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .csrf(csrf -> csrf.disable())
