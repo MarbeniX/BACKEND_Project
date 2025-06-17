ae293ff6971daf47bec2a6b16815a338699f7ad2
Berlin Ortega <marlitros200002@gmail.com>
2025-06-17 13:25:35 -0600

    Update SecurityConfig.java

--- src/main/java/com/backend/projectbackend/util/security/SecurityConfig.java
+++ src/main/java/com/backend/projectbackend/util/security/SecurityConfig.java
@@ -37,7 +37,7 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/auth/**").permitAll()
+                        .requestMatchers("/api/auth/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .csrf(csrf -> csrf.disable())
